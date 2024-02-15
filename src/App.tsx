00485a36876f903eec6d6105024192ee23c01334
Vishwajeet Kumar <vishwajeet95cy@gmail.com>
2024-02-16 00:35:34 +0530

    initialCommit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div className="flex h-full justify-center items-center">
+      <h1 className=" text-red-500 ">This is Vite React Typescript template</h1>
+    </div>
+  );
+};
